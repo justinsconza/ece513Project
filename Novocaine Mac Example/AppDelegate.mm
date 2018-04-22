baa375cf17fd13157cfb62a30a52cfd0b7a41746
Justin Sconza <justinsconza@gmail.com>
2018-04-22 18:14:44 -0500

    messed around with normalizing the output but it sounded basically like it was over limited and over compressed so that doesn't work. fixed up the logic surrounding starting and stopping the recording of input and output signals to files.

--- Novocaine Mac Example/AppDelegate.mm
+++ Novocaine Mac Example/AppDelegate.mm
@@ -60,7 +60,7 @@
     __block int D = 2;                  // delay amount
     __block float delta = 0.01;         // gradient step size, very sensitive
     __block float zeros = 0.0;          // for filling arrays
-    __block float volume = 64.0;        // scaling the final output back up, very sensitive
+    __block float volume = 256.0;        // scaling the final output back up, very sensitive
     
     // from 1024 containing both channels at once to two arrays of 512 for L and R separate
     float* deInterleavedL;
@@ -84,7 +84,7 @@
     
     NSArray *pathComponentsInput = [NSArray arrayWithObjects:
                                [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
-                               @"TDAF_input.wav",
+                               @"/ECE513/project/coreAudioOutput/TDAF_input.wav",
                                nil];
     NSURL *outputFileURL_input = [NSURL fileURLWithPathComponents:pathComponentsInput];
     
@@ -100,7 +100,7 @@
     
     NSArray *pathComponentsOutput = [NSArray arrayWithObjects:
                                [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
-                               @"TDAF_output.wav",
+                               @"/ECE513/project/coreAudioOutput/TDAF_output.wav",
                                nil];
     NSURL *outputFileURL_output = [NSURL fileURLWithPathComponents:pathComponentsOutput];
     
@@ -112,6 +112,9 @@
     __block int outputCounter = 0;
     __block int stopOutputFlag = 0;
     
+    // for normalizing output
+    __block float maximum;
+    
     // ------------------------------------------------------------------------ //
     // ------------- input callback ------------------------------------------- //
     // ------------------------------------------------------------------------ //
@@ -120,11 +123,26 @@
         
         // deinterleave and write to input ring buffer
         cblas_scopy(N, data, 2, deInterleavedL, 1);
+        // cblas_sscal(N, 1.0/cblas_snrm2(N,deInterleavedL,1),deInterleavedL,1);
         wself.ringBufferIn->AddNewFloatData(deInterleavedL, numFrames, 0);
-        
         
         // ---------- write input file ------------------------------------- //
         
+        if (!stopInputFlag) {
+            
+            if (inputCounter > 1000){
+                stopInputFlag = 1;
+                printf("Stopped writing input.\n");
+                [wself.inputFileWriter stop];
+            }
+            
+            else {
+                [wself.inputFileWriter writeNewAudio:data numFrames:numFrames numChannels:numChannels];
+                inputCounter += 1;
+            }
+        }
+        
+        /*
         [wself.inputFileWriter writeNewAudio:data numFrames:numFrames numChannels:numChannels];
         inputCounter += 1;
         
@@ -137,7 +155,7 @@
             // wself.audioManager.inputBlock = nil;
             [wself.inputFileWriter stop];
         }
-        
+        */
     }];
     
     // ------------------------------------------------------------------------ //
@@ -214,14 +232,6 @@
     vDSP_vfill(&zeros, delayedBlock, 1, L);
     
     
-    // ----- recording output -------- //
-    
-    __block int nRecord = 0;
-    __block int recordStart = 15*N;
-    __block int recordStop = recordStart+30*N;
-    float* recording;
-    recording = (float*)malloc((recordStop-recordStart) * sizeof(float));
-    
     
     __block int popCounter = 5;
     __block int doneCounting = 0;
@@ -253,7 +263,7 @@
                 cblas_scopy(L-1, convolutionOutput + N, 1, overlap, 1);                     // store overlap for next time
                 
                 
-                vDSP_vsmul(bufferOutL, 1, &volume, bufferOutL, 1, N);
+//                vDSP_vsmul(bufferOutL, 1, &volume, bufferOutL, 1, N);
                 
                 // -------- do the wiener update stuff --------------------------------------- //
                 
@@ -279,24 +289,20 @@
                 
                 // -------- prep for output -------------------------------------------------- //
                 
+//                cblas_sscal(N, 1.0/cblas_snrm2(N,bufferOutL,1),bufferOutL,1);
+                
+//                vDSP_maxv(bufferOutL, 1, &maximum, N);
+//                maximum = 0.25/maximum;
+//                vDSP_vsmul(bufferOutL, 1, &maximum, bufferOutL, 1, N);
+                
+                vDSP_vsmul(bufferOutL, 1, &volume, bufferOutL, 1, N);
+                
                 cblas_scopy(N, bufferOutL, 1, reInterleaved, 2);                            // reinterleaved left channel
                 cblas_scopy(N, bufferOutL, 1, reInterleaved+1, 2);                          // reinterleaved right channel
                 
-                /*
-                if(nRecord >= recordStart & nRecord < recordStop ) {
-                    cblas_scopy(N, bufferOutL, 1, recording + (nRecord-recordStart), 1);
-                    // printf("%d\n",nRecord);
-                }
-                
-                if(nRecord == recordStop) {
-                    writeOutput(recording, recordStop - recordStart, 44100);
-                    printf("done recording\n");
-                }
-                nRecord+=N;
-                */
-                
-                
                 wself.ringBufferOut->AddNewInterleavedFloatData(reInterleaved, numFrames, numChannels);
+                
+
                 
             } // end while loop
             
@@ -306,11 +312,23 @@
         
  
         // ---------- write output file ------------------------------------ //
-                
-        [wself.outputFileWriter writeNewAudio:outData numFrames:numFrames numChannels:numChannels];
-        outputCounter += 1;
         
         // writing blocks of N samples at a time
+        if (!stopOutputFlag) {
+            
+            if (outputCounter > 1000){
+                stopOutputFlag = 1;
+                printf("Stopped Writing Output.\n");
+                [wself.outputFileWriter stop];
+            }
+            
+            else {
+                [wself.outputFileWriter writeNewAudio:outData numFrames:numFrames numChannels:numChannels];
+                outputCounter += 1;
+            }
+        }
+        
+        /*
         if (outputCounter > 1000){
             if (!stopOutputFlag){
                 stopOutputFlag = 1;
@@ -319,6 +337,11 @@
             // wself.audioManager.inputBlock = nil;
             [wself.outputFileWriter stop];
         }
+        else {
+            [wself.outputFileWriter writeNewAudio:outData numFrames:numFrames numChannels:numChannels];
+            outputCounter += 1;
+        }
+        */
         
     }];
     
